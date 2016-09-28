e43348a39e32434ca395f136ad35ba02e28759ec
林達也 <t.hayashi0077+github@gmail.com>
2016-09-28 23:14:48 +0900

    Initial Commit

---
+++ AbstractPlayer/AbstractPlayer.h
@@ -0,0 +1,19 @@
+//
+//  AbstractPlayer.h
+//  AbstractPlayer
+//
+//  Created by 林達也 on 2016/09/28.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbstractPlayer.
+FOUNDATION_EXPORT double AbstractPlayerVersionNumber;
+
+//! Project version string for AbstractPlayer.
+FOUNDATION_EXPORT const unsigned char AbstractPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbstractPlayer/PublicHeader.h>
+
+
